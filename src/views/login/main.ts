1e56a0a11252fee658280cfa83d587a73477608e
sairu-sh <tmg.sairush@gmail.com>
2024-01-08 20:33:26 +0545

    linked the log in with the backend

--- src/views/login/main.ts
+++ src/views/login/main.ts
@@ -1,1 +1,30 @@
 import "../../style/style.css";
+import axios from "axios";
+
+const loginForm = document.getElementById("login-form");
+
+const http = axios.create({
+  baseURL: "http://localhost:8000/auth",
+});
+
+loginForm?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = (document.getElementById("email") as HTMLInputElement).value;
+  const password = (document.getElementById("password") as HTMLInputElement)
+    .value;
+  console.log(typeof password);
+
+  try {
+    const response = await http({
+      url: "/login",
+      data: {
+        email,
+        password,
+      },
+      method: "POST",
+    });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+});
--- src/views/register/main.ts
+++ src/views/register/main.ts
@@ -1,3 +1,4 @@
+import "../../style/style.css";
 import axios from "axios";
 
 const signupForm = document.getElementById("signup-form");
